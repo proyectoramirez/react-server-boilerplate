23dbf8d560728fc60b924f3d5e89c4286beed869
Jose Ramirez <proyectoramirez07@gmail.com>
2019-03-16 18:41:18 -0400

    Added frontend folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const api = require("./api");
+const frontEnd = require("./frontend");
 
 const router = express.Router();
 
 router.use("/api", api);
+router.use("/app", frontEnd);
 router.use("/", express.static("public"));
 
 module.exports = router;
